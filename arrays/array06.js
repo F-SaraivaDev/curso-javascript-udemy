03e2e2b0a92161566033872fd7eb601bd885ff83
F-SaraivaDev <saraivafds@gmail.com>
2023-05-22 20:41:04 -0300

    Aula 90 reverse(), reduce() + Desafio

--- arrays/array06.js
+++ arrays/array06.js
@@ -1,3 +1,28 @@
-let vetor = [1,2,3]
+let vetor = [1, 2, 3, 4]
+//console.log(vetor)
+//console.log(vetor.reverse())
+
+let j = 0
+let soma = vetor.reduce(function (acumulador, atual, i, vetor_) {
+   console.log("i: ", i)
+   console.log("j: ", j++)
+   //console.log(vetor_)
+   console.log("acumulador: ", acumulador, " --- atual: ", atual)
+   return acumulador + atual
+}, 0)
+
+console.log(soma)
 console.log(vetor)
-console.log(vetor.reverse())
+
+const nomes = ["Daniel", "Maria", "Joana", "João"]
+let nomesPorOrdem = nomes.reduce(function (nomes, nomeAtual) {
+   let primeiraLetra = nomeAtual[0]
+   if (nomes[primeiraLetra]) {
+      nomes[primeiraLetra]++
+   } else {
+      nomes[primeiraLetra] = 1
+   }
+   return nomes
+}, {})
+
+console.log(nomesPorOrdem)

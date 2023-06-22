2594d287cd904f635fd7173e90e4cf26853725fb
F-SaraivaDev <saraivafds@gmail.com>
2023-06-22 13:21:37 -0300

    prototype, call() e apply()

---
+++ arrays/desafio_ES5.js
@@ -0,0 +1,20 @@
+function sum(){
+	const numbers = []
+/*	for(let i = 0; i < arguments.length; i++){
+		numbers.push(arguments[i])
+	}*/
+	Array.prototype.forEach.call(arguments,function(argument){
+		numbers.push(argument)
+	})
+	return numbers.reduce(function(sum, atual){
+		return sum + atual
+	},0)
+}
+
+function avarege(){
+	const soma = sum.apply(null, arguments)
+	console.log(soma)
+	return soma / arguments.length
+}
+
+console.log(avarege(1,2,3,4,5))
